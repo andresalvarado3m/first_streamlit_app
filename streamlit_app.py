867c0b14217f06d56b322f0e0066069608df1e52
andresalvarado3m <128938529+andresalvarado3m@users.noreply.github.com>
2023-03-26 20:20:58 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,28 +43,30 @@ try:
 
 except URLError as e:
   streamlit.error()
-#fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-#streamlit.write('The user entered ', fruit_choice)
 
+streamlit.header("The fruit load list contains:")  
+#snowflake-related functions
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("select * from fruit_load_list")  
+    return my_cur.fetchall()
 
-#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-# removed line: streamlit.text(fruityvice_response.json()) #just writes the data to the screen
+#add a button to load the fruit 
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
 
-# take the json version of the response and normalize it
-#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output it the screen as a table
-#streamlit.dataframe(fruityvice_normalized)
-#don't run anything past here while we troubleshoot
 streamlit.stop()
 
 
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#my_cur = my_cnx.cursor()
+#my_cur.execute("select * from fruit_load_list")
+#my_data_rows = my_cur.fetchall()
+#streamlit.header("The fruit load list contains:")
+#streamlit.dataframe(my_data_rows)
 
 fruit_choice_to_add = streamlit.text_input('What fruit would you like to add','jackfruit')
 streamlit.write('Thanks for adding ', fruit_choice_to_add)
